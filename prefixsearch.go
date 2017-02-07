4f183b397803f0b882235316b314e2903516c676
Valerij <v.epaneshnikov@fasten.com>
2017-02-07 15:15:21 +0300

    Implented storing multiple values

--- prefixsearch.go
+++ prefixsearch.go
@@ -13,7 +13,7 @@ type SearchTree struct {
 }
 
 type node struct {
-	value    interface{}
+	values    []interface{}
 	childnum uint
 	childs   map[rune]*node
 }
@@ -47,7 +47,7 @@ func (tree *SearchTree) Add(key string, value interface{}) {
 	if needUpdate {
 		current.childnum++
 	}
-	current.value = value
+	current.values = append(current.values, value)
 }
 
 // AutoComplete returns autocomplete suggestions for given prefix
@@ -64,16 +64,18 @@ func (tree *SearchTree) AutoComplete(prefix string) []interface{} {
 
 	// we have found, now very stupid tree walk :)
 	result := make([]interface{}, 0, current.childnum)
-	current.recurse(func(v interface{}) {
+	current.recurse(func(v []interface{}) {
 		if nil != v {
-			result = append(result, v)
+			for _, val := range v {
+				result = append(result, val)
+			}
 		}
 	})
 	return result
 }
 
 // Search searches for value of key
-func (tree *SearchTree) Search(key string) interface{} {
+func (tree *SearchTree) Search(key string) []interface{} {
 	current := tree.root
 	for _, sym := range key {
 		var ok bool
@@ -82,11 +84,11 @@ func (tree *SearchTree) Search(key string) interface{} {
 			return nil
 		}
 	}
-	return current.value
+	return current.values
 }
 
-func (n *node) recurse(callback func(interface{})) {
-	callback(n.value)
+func (n *node) recurse(callback func([]interface{})) {
+	callback(n.values)
 	for _, v := range n.childs {
 		v.recurse(callback)
 	}
